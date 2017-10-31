dbcb85f26fdb0e83b7552c36d5132904f734ca8b
gokhantamkoc <gokhantamkoc@gmail.com>
2017-10-31 20:43:51 +0300

    added new features in es7

---
+++ app/index.js
@@ -0,0 +1,18 @@
+// ES7 New Features
+
+// 1) A New Operator
+
+let a = Math.pow(2, 5);
+console.log(a);
+
+// in ES7 there is a simple for doing this
+let b = 2 ** 5;
+console.log(b);
+
+// 2) Includes does work for Arrays as well
+
+let inc = 'wonderful'.includes('wonder');
+console.log(inc);
+
+let inc2 = [1,2,3,4,5].includes(4);
+console.log(inc);
